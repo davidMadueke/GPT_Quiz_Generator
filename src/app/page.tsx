28e0374a69d627516052ad048fd7b4fc7f4998b0
David Madueke <davidmadueke@gmail.com>
2023-09-03 22:47:13 +0100

    Configured Prisma DB

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button"
 
 
 export default function Home() {
   return (
     <>
       <h1 className="text-red-800">Hello World</h1>
+      <Button> Hello World</Button>
     </>
   )
 }
