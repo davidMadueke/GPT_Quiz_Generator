de5237e0cf91b4040542369466fdde2123d15365
David Madueke <davidmadueke@gmail.com>
2023-09-15 16:03:25 +0100

    Created helper func to generate Time taken from DB

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,23 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatTimeDelta(seconds: number){
+  const hours= Math.floor(seconds / 3600)
+  const minutes = Math.floor((seconds - (hours * 3600)) / 60)
+  const secs = seconds - (hours * 3600) - (minutes * 60)
+
+  const parts = []
+
+  if(hours>0){
+    parts.push(` ${hours}hrs`)
+  }
+  if(minutes>0){
+    parts.push(` ${minutes}mins`)
+  }
+  if(secs>0){
+    parts.push(` ${secs}secs`)
+  }
+
+  return parts.join(" ")
+}
