b930c00b82566b7ed8dd2a956f9ad50f64de9b74
David Madueke <davidmadueke@gmail.com>
2023-09-15 16:59:04 +0100

    Edited next config js file

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    images: {
+        domains: ["1h3.googleusercontent.com"]
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    }
 }
 
 module.exports = nextConfig
